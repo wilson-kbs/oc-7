08b686f6579f900623e24cb36954441dcb826397
Wilson <kabalisa.wilson@gmail.com>
2023-11-24 06:00:37 +0100

    refactor: change search function for use native loop

--- src/utils/searchRecipes.ts
+++ src/utils/searchRecipes.ts
@@ -14,40 +14,70 @@ type SearchRecipesParams = {
 
 export function searchRecipes(data: Recipe[], { input, tags: filters }: SearchRecipesParams): Recipe[] {
 	const normalizedInput = normalize(input);
+	const result = [];
+	const normalizedFilters: Required<Filters> = {
+		ingredients: [],
+		appliance: [],
+		ustensils: []
+	};
 
-	return data.reduce<Recipe[]>((arr, recipe) => {
-		let validText = false;
+	if (filters.ingredients) {
+		for (let i = 0; i < filters.ingredients.length; i++) {
+			normalizedFilters.ingredients.push(normalize(filters.ingredients[i]));
+		}
+	}
+	if (filters.appliance) {
+		for (let i = 0; i < filters.appliance.length; i++) {
+			normalizedFilters.appliance.push(normalize(filters.appliance[i]));
+		}
+	}
+	if (filters.ustensils) {
+		for (let i = 0; i < filters.ustensils.length; i++) {
+			normalizedFilters.ustensils.push(normalize(filters.ustensils[i]));
+		}
+	}
 
-		if (normalizedInput.length === 0)
-			validText = true;
-		else if ([recipe.name, recipe.description].some((text) => normalize(text).includes(normalizedInput)))
-			validText = true;
+	for (let i = 0; i < data.length; i++) {
+		const recipe = data[i];
+		let validText = normalizedInput.length === 0
+			|| normalize(recipe.name).includes(normalizedInput)
+			|| normalize(recipe.description).includes(normalizedInput);
+		let ingredientFilterCount = 0;
 
-		const ingredientsState = recipe.ingredients.reduce((acc, ingredient) => {
-			return {
-				validText: acc.validText || normalize(ingredient.ingredient).includes(normalizedInput),
-				validFilterCount: acc.validFilterCount + (filters.ingredients?.some(filter => normalize(ingredient.ingredient) === normalize(filter)) ? 1 : 0),
+		for (let j = 0; j < recipe.ingredients.length; j++) {
+			const ingredient = normalize(recipe.ingredients[j].ingredient);
+			if (!validText && ingredient.includes(normalizedInput)) {
+				validText = true;
 			}
-		}, {validText, validFilterCount: 0});
+			for (let k = 0; k < normalizedFilters.ingredients.length; k++) {
+				if (ingredient === normalizedFilters.ingredients[k]) {
+					ingredientFilterCount++;
+					break;
+				}
+			}
+		}
 
-		if (ingredientsState.validText)
-			validText = true;
+		if (ingredientFilterCount !== normalizedFilters.ingredients.length) {
+			continue;
+		}
 
-		if (ingredientsState.validFilterCount !== filters.ingredients?.length)
-			return arr;
+		let applianceMatch = !normalizedFilters.appliance.length || normalizedFilters.appliance.includes(normalize(recipe.appliance));
 
-		if (filters.appliance && filters.appliance.length > 0)
-			if (!filters.appliance.some(filter => normalize(recipe.appliance) === normalize(filter)))
-				return arr;
+		let ustensilMatch = !normalizedFilters.ustensils.length;
+		for (let j = 0; j < recipe.ustensils.length && !ustensilMatch; j++) {
+			let ustensil = normalize(recipe.ustensils[j]);
+			for (let k = 0; k < normalizedFilters.ustensils.length; k++) {
+				if (ustensil === normalizedFilters.ustensils[k]) {
+					ustensilMatch = true;
+					break;
+				}
+			}
+		}
 
+		if (applianceMatch && ustensilMatch && validText) {
+			result.push(recipe);
+		}
+	}
 
-		if (filters.ustensils && filters.ustensils.length > 0)
-			if (!filters.ustensils.every(filter => recipe.ustensils.some(ustensil => normalize(ustensil) === normalize(filter.toLowerCase()))))
-				return arr;
-
-		if (validText)
-			arr.push(recipe);
-
-		return arr;
-	}, [])
+	return result;
 }
